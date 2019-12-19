f074f97fdd72231d26694b7b6e310c18eecf4a88
limmabean <mlim45@gatech.edu>
2019-12-19 13:59:12 -0500

    Got config file working

--- gatsby_website/src/components/header.js
+++ gatsby_website/src/components/header.js
@@ -1,32 +1,50 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import {IconContext} from "react-icons";
+import {FaLinkedin, FaTwitter, FaGithub, FaFacebook, FaYoutube} from "react-icons/fa";
+import React from "react";
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: '#FFFFFF',
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: '#05386B',
             textDecoration: `none`,
           }}
         >
           {siteTitle}
         </Link>
       </h1>
+      <IconContext.Provider value={{size: "2em"}}>
+        <div>
+          <a href="https://www.linkedin.com/in/lim-matthew/">
+            <FaLinkedin/>
+          </a>
+          <a href="https://twitter.com/limma_bean">
+            <FaTwitter/>
+          </a>
+          <a href="https://github.com/limmabean">
+            <FaGithub/>
+          </a>
+          <a href="https://www.facebook.com/Mathlimma">
+            <FaFacebook/>
+          </a>
+          <a href="https://www.youtube.com/watch?v=9-CsmOtciTA&list=PL8C0vBBolbXEsU61g07w9NsHZ-2lq4UCD">
+            <FaYoutube/>
+          </a>
+        </div>
+      </IconContext.Provider>
     </div>
   </header>
 )
--- gatsby_website/src/pages/index.js
+++ gatsby_website/src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hello!</h1>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
