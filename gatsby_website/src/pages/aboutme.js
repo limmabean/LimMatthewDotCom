2fd50112c005156e28a43a468d190c3210ad4df5
limmabean <mlim45@gatech.edu>
2019-12-21 10:51:22 -0500

    update one more

--- gatsby_website/src/pages/aboutme.js
+++ gatsby_website/src/pages/aboutme.js
@@ -4,23 +4,11 @@ import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
 
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(theme => ({
-  main: {
-    margin: `0 0`,
-    maxWidth: 960,
-    padding: `0px 1.0875rem 1.45rem`,
-    paddingTop: 0,
-  }
-}));
-
 export default function aboutMePage() {
-  const classes = useStyles();
   return (
     <Layout>
       <SEO title="About Me"/>
-        <div className={classes.main}>
+        <div className="area">
           <Image/>
         </div>
     </Layout>
